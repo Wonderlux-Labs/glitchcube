549f5c5257afe1c1c6252b60201a3a7f41482395
Eric Stiens <estiens@users.noreply.github.com>
2025-08-12 22:14:03 -0500

    linting

--- db/migrate/016_create_summaries.rb
+++ db/migrate/016_create_summaries.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class CreateSummaries < ActiveRecord::Migration[7.1]
-  def change
-    create_table :summaries do |t|
+  def up
+    if table_exists?(:summaries)
+      return # Skip the change block if the table already exists
+    end
+
+    create_table :summaries, if_not_exists: true do |t|
       t.string :summary_type, null: false # personal, interaction, event, daily
       t.string :period, null: false # hourly, daily
       t.text :content, null: false
@@ -14,5 +18,9 @@ class CreateSummaries < ActiveRecord::Migration[7.1]
     add_index :summaries, :period
     add_index :summaries, :created_at
     add_index :summaries, %i[summary_type period created_at]
+  end
+
+  def down
+    # who needs down
   end
 end
